37a1493d2499c4f96c4390997f4f49362549af43
Joshua Bell <39743780+libertybell95@users.noreply.github.com>
2018-11-03 22:54:31 -0500

    Added function header

--- 7Cav/ACE Medical/vitalsCheck.js
+++ 7Cav/ACE Medical/vitalsCheck.js
@@ -1,5 +1,20 @@
 function vitalsCheck(HR, BPS, BPD) {
-  //Reference Document: https://docs.google.com/document/d/1S3bOxak-3MGunz_t46ePeMrGgpId1cbQAT5yekX7xa0/edit?usp=sharing
+  /*
+  /*
+  Name: vitalsCheck
+  Author: Joshua Bell (joshuakbell@gmail.com)
+  Reference Document: https://docs.google.com/document/d/1S3bOxak-3MGunz_t46ePeMrGgpId1cbQAT5yekX7xa0/edit?usp=sharing
+  Description: Checks vitals provided and asseses risk for cardiac arrest in ACE 3 Medical. NOT FOR REAL LIFE APPLICATION, FOR USE IN ARMA 3 ONLY. Built for integration into Google Home Mini smart assistant.
+  
+  Input:
+    0: Heart Rate [Number]
+    1: Blood Pressure - Systolic [Number]
+    2: Blood Pressure - Diastolic [Number]
+    
+  Output:
+    0: Vitals report. If all signs are normal then function will say all normal. If risk for cardiac arrest is high function will say what components are attributing to the heightened risk. If patient is in cardiac arrest function will command to begin CPR and say what cause patient to go into cardiac arrest [String]
+  
+  *
   
   var HRLevel; //Heart Rate. Levels are: Very Low, Low, Normal, High, Very High, Extra High
   var BPSLevel; //Blood Pressure (Systolic). Levels are: Very Low, Low, Normal, High, Very High
